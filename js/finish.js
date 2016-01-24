4038c05d8602a387be20707f2fd345aaf042da21
BrookeH <Brooke@Brookes-MBP.home>
2016-01-24 15:19:13 +0000

    Time stamp added

--- js/finish.js
+++ js/finish.js
@@ -1,14 +1,18 @@
 $(function() {
+
+        d = new Date();
+        date = d.toUTCString()
         text1ID = readCookie("text1ID")
         text1Counter = readCookie("text1Counter")
         text2Counter = readCookie("text2Counter")
     	text1LH = readCookie("text1LH")
         text2ID = readCookie("text2ID")
         text2LH = readCookie("text2LH")
-        img = localStorage.getItem("signature")
+        signature = localStorage.getItem("signature")
         text2Score = readCookie("text2Score")
         text1Score = readCookie("text1Score")
-        $("#img").val(img)
+        $("#date").val(date)
+        $("#signature").val(signature)
         $("#text1ID").val(text1ID)
         $("#text1Counter").val(text1Counter)
         $("#text2Counter").val(text2Counter)
